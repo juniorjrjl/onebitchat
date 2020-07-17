2910012ee0f0343e1372e4242c4aed84d2053489
Jose Luiz Junior <junior.jr.jl@gmail.com>
2020-07-17 09:32:32 -0300

    configuração do cancancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  before_action :authenticate_user!
-  include CanCan::ControllerAdditions
+	protect_from_forgery with: :exception
+	before_action :authenticate_user!
+	include CanCan::ControllerAdditions
+	rescue_from CanCan::AccessDeined do |e|
+		respond_to do |f|
+			f.json { head :forbidden, content_type: 'text/html' }
+			f.html { redirect_to main_app.root_url, notice: e.message }
+		end
+	end
 end
